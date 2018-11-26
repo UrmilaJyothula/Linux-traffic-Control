5db9ed28ca65b407e73e609904958972b5331424
UrmilaJyothula <35899608+UrmilaJyothula@users.noreply.github.com>
2018-11-26 18:00:08 +0100

    Create AutoDiscovery_interfaces.py

---
+++ AutoDiscovery_interfaces.py
@@ -0,0 +1,10 @@
+#! /usr/bin/python
+
+# this python script automatically detect new interfaces in the host system
+
+import os, subprocess, time
+while ():
+  cmd = subprocess_checkoutput("ls /system/class/net/", shell=True)
+  interfaces = []
+  interfaces = cmd.splitlines()
+  time.sleep(5)
